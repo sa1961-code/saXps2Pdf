ed6d819d7b95eaf54207a7d29a6c8c8cd6760068
Steffen Anders <info@steffen-anders.de>
2024-02-06 15:49:17 +0100

    V01.24.02.00

--- bin/Release/license/README.txt
+++ bin/Release/license/README.txt
@@ -4,13 +4,14 @@ saXps2PdfLib32.dll, saXps2PdfLib64.dll and saXps2Pdf.exe are a simple interactiv
 userinterface for the GhostXPS document conversion engine for Microsoft Windows.
 Also provides a COM-interface, see saXps2Pdf.tlb or saXps2PdfLib.cs.
 
-saXps2Pdf is distributed under the terms of the GNU GPL.
+saXps2Pdf is licensed under the terms of the GNU GPL.
 
-Source code and other binaries can be obtained from http://steffen-anders.de/saXps2Pdf/
+Source code and other binaries can be obtained from https://github.com/sa1961-code/saXps2Pdf
 
 == GhostXPS ==
 
 Source code and other binaries can be obtained from http://ghostscript.com/
+The included GhostXPS modules are licensed under the terms of the GNU AGPL.
 
 GhostXPS, GhostPCL, Ghostscript and the urw font set are also available
 for commercial licensing under other terms. Please visit http://artifex.com/ for more information.
--- saXps2Pdf/XpsViewer_Form.Designer.cs
+++ saXps2Pdf/XpsViewer_Form.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XpsViewer_Form));
             this.textBoxMessage = new System.Windows.Forms.TextBox();
             this.buttonOpen = new System.Windows.Forms.Button();
             this.buttonPrint = new System.Windows.Forms.Button();
@@ -37,21 +38,23 @@
             // 
             // textBoxMessage
             // 
-            this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxMessage.Location = new System.Drawing.Point(12, 387);
+            this.textBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxMessage.Location = new System.Drawing.Point(9, 314);
+            this.textBoxMessage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxMessage.Name = "textBoxMessage";
             this.textBoxMessage.ReadOnly = true;
-            this.textBoxMessage.Size = new System.Drawing.Size(393, 22);
+            this.textBoxMessage.Size = new System.Drawing.Size(296, 20);
             this.textBoxMessage.TabIndex = 0;
             this.textBoxMessage.TabStop = false;
             // 
             // buttonOpen
             // 
             this.buttonOpen.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonOpen.Location = new System.Drawing.Point(422, 383);
+            this.buttonOpen.Location = new System.Drawing.Point(316, 311);
+            this.buttonOpen.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonOpen.Name = "buttonOpen";
-            this.buttonOpen.Size = new System.Drawing.Size(110, 31);
+            this.buttonOpen.Size = new System.Drawing.Size(82, 25);
             this.buttonOpen.TabIndex = 1;
             this.buttonOpen.Text = "Xps öffnen";
             this.buttonOpen.UseVisualStyleBackColor = true;
@@ -61,9 +64,10 @@
             // 
             this.buttonPrint.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonPrint.Enabled = false;
-            this.buttonPrint.Location = new System.Drawing.Point(538, 383);
+            this.buttonPrint.Location = new System.Drawing.Point(404, 311);
+            this.buttonPrint.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonPrint.Name = "buttonPrint";
-            this.buttonPrint.Size = new System.Drawing.Size(110, 31);
+            this.buttonPrint.Size = new System.Drawing.Size(82, 25);
             this.buttonPrint.TabIndex = 2;
             this.buttonPrint.Text = "Xps drucken";
             this.buttonPrint.UseVisualStyleBackColor = true;
@@ -73,9 +77,10 @@
             // 
             this.buttonPDF.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonPDF.Enabled = false;
-            this.buttonPDF.Location = new System.Drawing.Point(654, 383);
+            this.buttonPDF.Location = new System.Drawing.Point(490, 311);
+            this.buttonPDF.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonPDF.Name = "buttonPDF";
-            this.buttonPDF.Size = new System.Drawing.Size(110, 31);
+            this.buttonPDF.Size = new System.Drawing.Size(82, 25);
             this.buttonPDF.TabIndex = 3;
             this.buttonPDF.Text = "Pdf speichern";
             this.buttonPDF.UseVisualStyleBackColor = true;
@@ -83,26 +88,29 @@
             // 
             // elementHost1
             // 
-            this.elementHost1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.elementHost1.Location = new System.Drawing.Point(12, 12);
+            this.elementHost1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.elementHost1.Location = new System.Drawing.Point(9, 10);
+            this.elementHost1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.elementHost1.Name = "elementHost1";
-            this.elementHost1.Size = new System.Drawing.Size(752, 351);
+            this.elementHost1.Size = new System.Drawing.Size(564, 285);
             this.elementHost1.TabIndex = 4;
             this.elementHost1.Text = "elementHost1";
             this.elementHost1.Child = null;
             // 
             // XpsViewer_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 430);
+            this.ClientSize = new System.Drawing.Size(582, 349);
             this.Controls.Add(this.elementHost1);
             this.Controls.Add(this.buttonPDF);
             this.Controls.Add(this.buttonPrint);
             this.Controls.Add(this.buttonOpen);
             this.Controls.Add(this.textBoxMessage);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "XpsViewer_Form";
             this.Text = "Xps in Pdf konvertieren";
             this.ResumeLayout(false);
